17bcc2048cd61831cc54593c9f495cdcf24a4893
sanjay26 <sanjay@okrasolar.com>
2020-04-04 19:24:02 +0530

    read state and display with conditional loading

--- src/App.js
+++ src/App.js
@@ -5,18 +5,39 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      users: []
+      users: [],
+      loading: false
     }
   }
 
+getUsers(){
+  this.setState({
+    loading: true
+  })
+  axios('https://api.randomuser.me/?nat=US&results=5').then(response => this.setState({
+    users : response.data.results,
+    loading: false
+  })
+  );
+}
+
   componentWillMount(){
-    axios('https://api.randomuser.me/?nat=US&results=5').then(response => this.setState({
-      users : response.data.results
-    }))
+    this.getUsers();
   }
 
   render() {
-    return <div className = "App" >hello react!!</div>;
+    return (<div className = "App" >
+      {!this.state.loading 
+      ? this.state.users.map(user => (
+        <div>
+           <h3>{user.name.first + " " + user.name.last}</h3>
+            <p>{user.email}</p>
+            <hr />
+        </div>
+      ))
+      : 'Loading'}
+      </div>
+    );
   }
 }
 
